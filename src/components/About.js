ad722f5b2681ca607958ad0e6542683b278d06c9
AMOS KIPKORIR <amos.kipkorir@student.moringaschool.com>
2025-07-12 03:08:13 +0300

    Update components and data: Home, About, NavBar, and data.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return( <div id="about">
+    <h2>About Me</h2>
+    <img src={image} alt="I made this" />
+    <p>
+      I am a passionate web developer with a focus on creating dynamic and responsive web applications.
+      My journey in web development has been fueled by a love for coding and a desire to build impactful digital experiences.
+    </p>
+  </div>
+  );
 }
 
 export default About;
